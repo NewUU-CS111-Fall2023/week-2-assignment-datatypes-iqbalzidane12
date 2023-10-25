256a7f3c21e7fc52f9e7064495ec602426b0e8d6
iqbalzidane12 <147694869+iqbalzidane12@users.noreply.github.com>
2023-10-25 23:21:13 +0500

    Update task_2.h

--- src/cpp/task_2.h
+++ src/cpp/task_2.h
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <random>
 
+using namespace std;
+
+int guessing_file() {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 100);
+    int secret_number = dist(gen);
+    int guess, num_guesses = 0;
+       do {
+        cout << "Enter a guess: ";
+        cin >> guess;
+        num_guesses++;
+        if (guess == secret_number) {
+            cout << "Correct! You guessed the number in " << num_guesses << " guesses." << endl;
+            break;
+        } else if (guess < secret_number) {
+            cout << "Too low." << endl;
+        } else {
+            cout << "Too high." << endl;
+        }
+    } while (true);
+
+    return 0;
+}
